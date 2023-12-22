218741dc9f28426096aa5d2cf8c24e0d950de3ca
Anathan Wang <144335469+AnathanWang@users.noreply.github.com>
2023-12-22 10:51:39 +0300

    Add files via upload

---
+++ KR2.2.pas
@@ -0,0 +1,25 @@
+﻿var s,s1:string;
+    i,j,k,n:integer;
+begin
+writeln('Введите строку содержащую несколько одинаковых подстрок');
+s:=ReadLnString('Введите строку: ');
+var ps:=ReadLnString('Введите искомую подстроку: ');
+var t:='';
+i:=1;
+var c:=1;
+k:=0;
+var countPs:=0;
+while i<=Length(s) do begin
+   k:=0;
+   while ((i+k)<=Length(s)) and (k<Length(ps)) and (s[i+k]=ps[k+1]) do
+        inc(k);
+   if k=Length(ps) then Inc(countPs);
+   if (k=Length(ps)) and not odd(countPs) then inc(i,k)
+   else begin
+     t:=t+s[i];
+     inc(i);
+     end;
+   end;
+s:=t;
+writeln(s);
+end.
---
+++ KR2.pas
@@ -0,0 +1,14 @@
+﻿var
+A: array of integer;
+i, n: integer;
+begin
+writeln('Enter the size of the array:');
+readln(n);
+SetLength(A, n);
+for i := 0 to n - 1 do
+read(A[i]);
+if (n <= 1) or (A[0] > A[1]) or (A[n - 2] > A[n - 1]) then
+writeln('The array is not decreasing.')
+else
+writeln('The array is decreasing.');
+end.
